cf49f537c051bf637d7004a847dd641c92989655
Decentralife <christopherdonaldmanning@gmail.com>
2021-11-08 05:07:39 -0500

    added files to repo

---
+++ src/data/constants/constants.js
@@ -0,0 +1,4 @@
+//Token Address
+export const GDAOAddress = "0xC155d6B013bD9C96Fe132F7a19964E931441a1E2";
+//MerkleDistributor Address
+export const rewardPoolAddress = "0xa8c20Ec19F4e84099029798aC11b32C768BF9780";
